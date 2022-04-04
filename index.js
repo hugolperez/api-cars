c02c2e46aa40f64bc9621c1ab3ba9cc9116369a3
Hugo Leonel Pérez <68658193+hugolperez@users.noreply.github.com>
2022-04-04 17:49:10 -0600

    modificación del mensaje

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ const carsApi = require("./routes/cars");
 carsApi(app);
 
 app.listen(config.port, () =>
-  console.log("Server listening on port, " + config.port)
+  console.log("Server listening on port" + config.port)
 );
